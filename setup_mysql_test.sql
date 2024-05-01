cd39c4824ce1940e80929836e82ac79a02c8b940
MartinScript <martinsojoola2@gmail.com>
2024-05-01 23:15:13 +0100

    fix bugs

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,4 @@
+-- Create test database and user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';

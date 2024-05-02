33a82f7ad0aab7a6fa70a8adffe6a4d4902c5312
MartinScript <martinsojoola2@gmail.com>
2024-05-02 01:18:06 +0100

    fix bugs

--- models/city.py
+++ models/city.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This is the city class"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Table, Column, String, Foreign_key
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """The city class, contains state ID and name"""
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
 
+    # initialize class for file/db storage type
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), Foreign_key("states.id"), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="cities")
